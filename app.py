ea02779ad17319780a3cabcfcc2c90522228ac8b
Mohd Nauman <102999353+MohdNauman@users.noreply.github.com>
2022-07-06 16:00:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+
+import numpy as np
+from flask import Flask, request, jsonify
+from flask import render_template 
+import pickle
+import sklearn
+
+app = Flask(__name__)
+model = pickle.load(open('model3.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    output = round(prediction[0])
+    
+    return render_template('index.html', prediction_text = 'Price {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    #For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == '__main__' :
+    app.run(debug=True)
+    
+
---
+++ request.py
@@ -0,0 +1,7 @@
+
+import requests
+
+url = 'http://localhost:5000/predict_api'
+output = requests.post(url,json={'cloud': 4000, 'furniture': 40000, 'equipment': 140000, 'software': 4000, 'salary_hr':2000,'length': 400, 'study_material_cost': 1500, 'rent': 90000, 'electricity': 16000, 'maintainance': 1500,'marketing': 2000, 'certification': 1, 'placement': 1})
+
+print(output.json())
