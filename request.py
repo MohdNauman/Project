190598c35e56d36c6ee95b24047fc4340905f559
Mohd Nauman <102999353+MohdNauman@users.noreply.github.com>
2022-07-06 16:08:25 +0530

    Update request.py

--- request.py
+++ request.py
@@ -2,6 +2,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-output = requests.post(url,json={'cloud': 4000, 'furniture': 40000, 'equipment': 140000, 'software': 4000, 'salary_hr':2000,'length': 400, 'study_material_cost': 1500, 'rent': 90000, 'electricity': 16000, 'maintainance': 1500,'marketing': 2000, 'certification': 1, 'placement': 1})
+output1 = requests.post(url,json={'cloud': 4000, 'furniture': 40000, 'equipment': 140000, 'software': 4000, 'salary_hr':2000,'length': 400, 'study_material_cost': 1500, 'rent': 90000, 'electricity': 16000, 'maintainance': 1500,'marketing': 2000, 'certification': 1, 'placement': 1})
 
-print(output.json())
+print(output1.json())
